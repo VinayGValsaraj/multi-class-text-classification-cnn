2d6eacbcb9ec0fdd88827b7e359d40976ec153a2
vinaygvalsaraj <vinaygvalsaraj@gmail.com>
2019-05-14 00:25:52 -0500

    Added testing dev immediately after

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ import data_helper
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.INFO)
 from tensorflow.contrib import learn
 from sklearn.metrics import accuracy_score, classification_report
 
@@ -13,13 +14,13 @@ logging.getLogger().setLevel(logging.INFO)
 
 def predict_unseen_data():
 	"""Step 0: load trained model and parameters"""
-	print ("loading")
+	# print ("loading")
 	params = json.loads(open('./parameters.json').read())
 	checkpoint_dir = sys.argv[1]
 	if not checkpoint_dir.endswith('/'):
 		checkpoint_dir += '/'
 	checkpoint_file = tf.train.latest_checkpoint(checkpoint_dir + 'checkpoints')
-	logging.critical('Loaded the trained model: {}'.format(checkpoint_file))
+	# logging.critical('Loaded the trained model: {}'.format(checkpoint_file))
 
 	"""Step 1: load data for prediction"""
 	test_file = sys.argv[2]
@@ -33,12 +34,9 @@ def predict_unseen_data():
 	np.fill_diagonal(one_hot, 1)
 	label_dict = dict(zip(labels, one_hot))
 
-	# x_raw = [example['text'] for example in test_examples]
-	# print (test_examples['text'])
-	# raise
 	x_raw = df['text'].apply(lambda x: str(x)).tolist()
 	x_test = [data_helper.clean_str(x) for x in x_raw]
-	logging.info('The number of x_test: {}'.format(len(x_test)))
+	# logging.info('The number of x_test: {}'.format(len(x_test)))
 
 	y_test = None
 	# if 'label' in test_examples[0]:
@@ -46,12 +44,12 @@ def predict_unseen_data():
 	y_raw = df['label'].tolist()
 	# y_raw = df['label'].apply(lambda y: y)]).tolist()
 	y_test = [label_dict[str(y)] for y in y_raw]
-	logging.info('The number of y_test: {}'.format(len(y_test)))
+	# logging.info('The number of y_test: {}'.format(len(y_test)))
 
 	vocab_path = os.path.join(checkpoint_dir, "vocab.pickle")
 	vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
 	x_test = np.array(list(vocab_processor.transform(x_test)))
-	print ("computing")
+	# print ("computing")
 	"""Step 2: compute the predictions"""
 	graph = tf.Graph()
 	with graph.as_default():
@@ -71,7 +69,7 @@ def predict_unseen_data():
 			for x_test_batch in batches:
 				batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
 				all_predictions = np.concatenate([all_predictions, batch_predictions])
-	print ("done computing")
+	# print ("done computing")
 	if y_test is not None:
 		y_test = np.argmax(y_test, axis=1)
 		correct_predictions = sum(all_predictions == y_test)
@@ -87,11 +85,13 @@ def predict_unseen_data():
 		# with open('./data/small_samples_prediction.json', 'w') as outfile:
 		# 	json.dump(df, outfile, indent=4)
 
-		logging.critical('The accuracy is: {}'.format(correct_predictions / float(len(y_test))))
-		logging.critical('The prediction is complete')
+		# logging.critical('The accuracy is: {}'.format(correct_predictions / float(len(y_test))))
+		# logging.critical('The prediction is complete')
 		print ("Accuracy: ", accuracy_score(y_raw, labels))
 		print (classification_report(y_raw, labels))
 
 if __name__ == '__main__':
 	# python3 predict.py ./trained_model_1478649295/ ./data/small_samples.json
+	predict_unseen_data()
+	sys.argv[2] = sys.argv[2].replace("dev", "test")
 	predict_unseen_data()
